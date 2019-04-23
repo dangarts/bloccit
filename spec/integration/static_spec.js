e0a9823dd0dd36410ca05bd73856dae5b0da9970
Tai Dang <tai.dangit@gmail.com>
2019-04-23 03:12:38 -0700

    assignment 2 intro express / complete

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -5,7 +5,15 @@ const base = "http://localhost:3000/";
 describe("routes : static", () => {
 
   describe("GET /", () => {
+    it("should return status code 200", (done) => {
+      request.get(base, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        done();
+      });
+    });
+  });
 
+  describe("GET /marco", () => {
     it("should return status code 200", (done) => {
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
