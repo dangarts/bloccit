72ddd0348568921c191ff3d5a22593b84477f8c9
Tai Dang <tai.dangit@gmail.com>
2019-05-04 15:15:40 -0700

    checkpoint 9 post 2 / complete

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -3,10 +3,9 @@ module.exports = {
     const staticRoutes = require("../routes/static");
     const postRoutes = require("../routes/posts");
     const topicRoutes = require("../routes/topics");
-    
+
     app.use(staticRoutes);
     app.use(postRoutes);
     app.use(topicRoutes);
-    
   }
 }
